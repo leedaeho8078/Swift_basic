7202b77fb7664167b97365393bd8a6a464a5719c
이대호 <dooho199234@nate.com>
2023-12-02 22:00:51 +0900

    고차 함수

---
+++ Swift_basic/Higher_order_function/Map.swift
@@ -0,0 +1,8 @@
+//
+//  Map.swift
+//  Swift_basic
+//
+//  Created by 이대호 on 2023/12/02.
+//
+
+import Foundation
---
+++ Swift_basic/Higher_order_function/filter.swift
@@ -0,0 +1,8 @@
+//
+//  filter.swift
+//  Swift_basic
+//
+//  Created by 이대호 on 2023/12/02.
+//
+
+import Foundation
